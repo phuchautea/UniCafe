0f1015a75fb60efde1b2af3c0237fedae83d8e25
Nam <vinhoainam0815@gmail.com>
2023-03-13 15:15:20 +0700

    Done Admin Category

--- UniCafe/Areas/Admin/Controllers/ManageCategoryController.cs
+++ UniCafe/Areas/Admin/Controllers/ManageCategoryController.cs
@@ -59,76 +59,77 @@ namespace UniCafe.Areas.Admin.Controllers
 
             return RedirectToAction("Index", "ManageCategory");
         }
-        //public ActionResult Edit(int? Id)
-        //{
-        //    if (Id == null)
-        //    {
-        //        return RedirectToAction("Index", "ManageCategory");
-        //    }
-        //    var checkId = GetById(Id.Value);
-        //    if (checkId == null)
-        //    {
-        //        return RedirectToAction("Index", "ManageCategory");
-        //    }
-        //    var categories = _categoryRepository.GetAll().ToList();
-        //    ViewBag.Categories = categories;
-        //    Category category = GetById(Id.Value);
-        //    return View(category);
-        //}
-        //[HttpPost]
-        //public ActionResult Edit(FormCollection formCollection)
-        //{
-        //    List<string> errors = new List<string>();
-        //    try
-        //    {
-        //        var id = formCollection["id"];
-        //        var name = formCollection["name"];
-        //        var slug = formCollection["slug"];
-        //        var description = formCollection["description"];
-        //        var status = formCollection["status"];
-        //        var checkSlug = Context.Categories.Count(x => x.Slug == slug);
-        //        var getCategoryContainsSlug = Context.Categories.FirstOrDefault(x => x.Slug == slug);
-        //        if (string.IsNullOrEmpty(name))
-        //        {
-        //            errors.Add("Chưa nhập tên loại sản phẩm");
-        //        }
-        //        if (checkSlug > 0 && getCategoryContainsSlug.Id != Convert.ToInt32(id))
-        //        {
-        //            errors.Add("Slug đã tồn tại");
-        //        }
-        //        if (string.IsNullOrEmpty(description))
-        //        {
-        //            errors.Add("Chưa nhập mô tả loại sản phẩm");
-        //        }
-        //        if (errors.Count == 0)
-        //        {
-        //            var product = GetById(Int32.Parse(formCollection["Id"]));
-        //            product.Name = name;
-        //            product.Slug = slug;
-        //            product.Description = description;
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        errors.Add(ex.Message);
-        //    }
-        //    TempData["Errors"] = errors;
-        //    return RedirectToAction("Index", "ManageCategory");
-        //}
-        //[HttpPost]
-        //public ActionResult Delete(int? Id)
-        //{
-        //    if (Id == null)
-        //    {
-        //        return Json(new { success = false, message = "ID không hợp lệ" });
-        //    }
-        //    Category category = GetById(Id.Value);
-        //    if (category == null)
-        //    {
-        //        return Json(new { success = false, message = "Loại sản phẩm không tồn tại" });
-        //    }
-        //    Remove(category);
-        //    return Json(new { success = true, message = "Xóa loại sản phẩm thành công" });
-        //}
+        public ActionResult Edit(int? Id)
+        {
+            if (Id == null)
+            {
+                return RedirectToAction("Index", "ManageCategory");
+            }
+            var checkId = GetById(Id.Value);
+            if (checkId == null)
+            {
+                return RedirectToAction("Index", "ManageCategory");
+            }
+            Category category = GetById(Id.Value);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Edit(FormCollection formCollection)
+        {
+            List<string> errors = new List<string>();
+            try
+            {
+                var id = formCollection["id"];
+                var name = formCollection["name"];
+                var slug = formCollection["slug"];
+                var description = formCollection["description"];
+                var status = formCollection["status"];
+                var checkSlug = Context.Categories.Count(x => x.Slug == slug);
+                var getCategoryContainsSlug = Context.Categories.FirstOrDefault(x => x.Slug == slug);
+                if (string.IsNullOrEmpty(name))
+                {
+                    errors.Add("Chưa nhập tên danh mục");
+                }
+                if (checkSlug > 0 && getCategoryContainsSlug.Id != Convert.ToInt32(id))
+                {
+                    errors.Add("Slug đã tồn tại");
+                }
+                if (string.IsNullOrEmpty(description))
+                {
+                    errors.Add("Chưa nhập mô tả danh mục");
+                }
+                if (errors.Count == 0)
+                {
+                    var category = GetById(Int32.Parse(formCollection["Id"]));
+                    category.Name = name;
+                    category.Slug = slug;
+                    category.Status = status;
+                    category.Description = description;
+                    category.UpdatedAt = DateTime.Now;
+                    Update(category);
+                }
+            }
+            catch (Exception ex)
+            {
+                errors.Add(ex.Message);
+            }
+            TempData["Errors"] = errors;
+            return RedirectToAction("Index", "ManageCategory");
+        }
+        [HttpPost]
+        public ActionResult Delete(int? Id)
+        {
+            if (Id == null)
+            {
+                return Json(new { success = false, message = "ID không hợp lệ" });
+            }
+            Category category = GetById(Id.Value);
+            if (category == null)
+            {
+                return Json(new { success = false, message = "Danh mục không tồn tại" });
+            }
+            Remove(category);
+            return Json(new { success = true, message = "Xóa danh mục thành công" });
+        }
     }
 }
