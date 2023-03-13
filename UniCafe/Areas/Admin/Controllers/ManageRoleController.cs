dfe42d7181f0b68d8dd48ecf13e7f9f1ed2c9039
Nam <vinhoainam0815@gmail.com>
2023-03-13 12:55:37 +0700

    update them

--- UniCafe/Areas/Admin/Controllers/ManageRoleController.cs
+++ UniCafe/Areas/Admin/Controllers/ManageRoleController.cs
@@ -15,7 +15,7 @@ namespace UniCafe.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class ManageRoleController : BaseController<Role>
     {
-        //test role nè
+        //test role nè tan luc
         // GET: Admin/ManageRole
         public ActionResult Index()
         {
