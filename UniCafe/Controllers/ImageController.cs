4c3de1087b608d45e937e8f94dd48f91d381661b
phuchau.tea <phuchau.tea@gmail.com>
2023-04-05 16:31:45 +0700

    feat: upload image

---
+++ UniCafe/Controllers/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniCafe.Controllers
+{
+    public class ImageController : Controller
+    {
+        [HttpPost]
+        public string ProcessUpload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return "";
+            }
+
+            var fileName = file.FileName;
+            var path = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
+            if (System.IO.File.Exists(path))
+            {
+                var extension = Path.GetExtension(fileName);
+                fileName = Path.GetFileNameWithoutExtension(fileName) + "_" + Guid.NewGuid().ToString() + extension;
+            }
+
+            file.SaveAs(Path.Combine(Server.MapPath("~/Content/images/"), fileName));
+            return "/Content/images/" + fileName;
+        }
+    }
+}
