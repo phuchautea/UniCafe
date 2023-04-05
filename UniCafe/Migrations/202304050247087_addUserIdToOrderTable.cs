2e126df1ad601012c485a61210ca85880223cd2e
Nam <vinhoainam0815@gmail.com>
2023-04-05 16:31:05 +0700

    update from master

---
+++ UniCafe/Migrations/202304050247087_addUserIdToOrderTable.cs
@@ -0,0 +1,18 @@
+﻿namespace UniCafe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addUserIdToOrderTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Orders", "UserId", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Orders", "UserId");
+        }
+    }
+}
